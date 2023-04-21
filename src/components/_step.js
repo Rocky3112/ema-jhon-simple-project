2b42fc34ccec7dcc20d85f89799294f12f65760b
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-04-21 23:59:15 +0600

    hosting done

--- src/components/_step.js
+++ src/components/_step.js
@@ -6,3 +6,19 @@
  * 5 build > Authentication > get started > enable
  * 6 set route signup and login
  * **/ 
+
+/**
+ * 1. create a context provider file
+ * 2.create a context and set provider
+ * 3.set the children props
+ * 4.set context value
+ * 5. set the provider
+ * 6.**/
+/**
+ * Hosting--------------
+ * 1.one time per computer
+ * npm install -g firebase-tools
+ * firebase login
+ * firebase init
+ * 
+ * */  
